af5323f42dbe80025c52aac40130d0e02300ab4e
Fedir03 <federicoruckauf@gmail.com>
2024-08-31 16:05:24 -0300

    Se hizo una primer implementacion del envio de nombres de archivos desde app a slave y la recepcion de los hash.

--- app.c
+++ app.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <string.h>
 
 // Informacion de slave
 #define SLAVE_AMMOUNT 10
@@ -19,8 +21,7 @@ int main(int argc, char const *argv[])
     char *const envpSlave[] = {NULL};
 
     // Esto es para el select
-    fd_set *readfds;
-    fd_set *writefds;
+    fd_set readfds;
 
     // Loop para hacer pipes, hacer los fork/execve y cargar read y write fds
     for (size_t i = 0; i < SLAVE_AMMOUNT; i++)
@@ -29,35 +30,50 @@ int main(int argc, char const *argv[])
         pipe(slaveToAppFds[i]);
         if (fork() == 0)
         {
-            close(appToSlaveFds[1]);
-            close(slaveToAppFds[0]);
-            dup2(appToSlaveFds[0], 0);
-            dup2(slaveToAppFds[1], 1);
-            close(appToSlaveFds[0]);
-            close(slaveToAppFds[1]);
+            close(appToSlaveFds[i][1]);
+            close(slaveToAppFds[i][0]);
+            dup2(appToSlaveFds[i][0], 0);
+            dup2(slaveToAppFds[i][1], 1);
+            close(appToSlaveFds[i][0]);
+            close(slaveToAppFds[i][1]);
             execve(SLAVEPATH, argvSlave, envpSlave);
         }
-        // VER COMO SE USA ESTO
-        // FD_SET(appToSlaveFds[0], readfds);
+        // carga readfds con todos los fds que necesitamos monitorear
+        FD_SET(appToSlaveFds[i][0], &readfds);
     }
 
     // Loop para mandar archivos a los esclavos y leer hashes. Usa select para ir viendo que fds estan listos
-    /*
-        //crear readfds y writefds con todos los fds que necesitamos monitorear
-        while (QUEDEN ARCHIVOS POR HASHEAR) {
-            //cargar readfds y writefds con todos los fds monitoreados
-            readfdsX = readfds
-            select(readfdsX, writefdsX);
 
-            readfdsX <- solo los disponibles
-
-            //en readfds y writefds quedan solo los fds habilitados para lectura y escritura respectivamente
-
-            //read from readable fds (LEER HASHES)
-            //write to writeable fds (MANDAR ARCHIVOS)
-
+    int i = 1;
+    fd_set readfdsX;
+    while (i < argc)
+    {
+        // Manda un archivo a cada esclavo
+        for (size_t j = 0; j < SLAVE_AMMOUNT && i < argc; j++)
+        {
+            send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
+            i++;
         }
-    */
+        // cargar readfds y writefds con todos los fds monitoreados
+        readfdsX = readfds;
+        select(100, &readfdsX, NULL, NULL, NULL);
+        // en readfds y writefds quedan solo los fds habilitados para lectura y escritura respectivamente
+        // read from readable fds (LEER HASHES) y send despues de leer (MANDAR ARCHIVOS)
+
+        for (size_t j = 0; j < SLAVE_AMMOUNT && i < argc; j++)
+        {
+            if (FD_ISSET(slaveToAppFds[j][0], &readfdsX))
+            {
+                // Test
+                char buf[100];
+                read(slaveToAppFds[j][0], buf, 100);
+                printf("%s\n", buf);
+                //
+                send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
+                i++;
+            }
+        }
+    }
 
     return 0;
 }
