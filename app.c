80da13fe47ff6f2e18cc14d2537175e33ddca6ac
leoWeitz <lweitz@itba.edu.ar>
2024-08-31 19:59:01 -0300

    Sigue desarrollando slave.c y modifica cosas menores de app.c

--- app.c
+++ app.c
@@ -22,10 +22,12 @@ int main(int argc, char const *argv[])
 
     // Esto es para el select
     fd_set readfds;
+    FD_ZERO(&readfds);
 
     // Loop para hacer pipes, hacer los fork/execve y cargar read y write fds
     for (size_t i = 0; i < SLAVE_AMMOUNT; i++)
     {
+        printf("Piping number %ld\n", i);
         pipe(appToSlaveFds[i]);
         pipe(slaveToAppFds[i]);
         if (fork() == 0)
@@ -33,44 +35,61 @@ int main(int argc, char const *argv[])
             close(appToSlaveFds[i][1]);
             close(slaveToAppFds[i][0]);
             dup2(appToSlaveFds[i][0], 0);
-            dup2(slaveToAppFds[i][1], 1);
+            //dup2(slaveToAppFds[i][1], 1);
             close(appToSlaveFds[i][0]);
             close(slaveToAppFds[i][1]);
             execve(SLAVEPATH, argvSlave, envpSlave);
         }
         // carga readfds con todos los fds que necesitamos monitorear
-        FD_SET(appToSlaveFds[i][0], &readfds);
+        FD_SET(slaveToAppFds[i][0], &readfds);
     }
+
+    //Testing
+    for (size_t j = 0; j < SLAVE_AMMOUNT ; j++) {
+
+        if (FD_ISSET(slaveToAppFds[j][0], &readfds)) {
+            printf("%d\n", slaveToAppFds[j][0]);
+        }
+    }
+    //
 
     // Loop para mandar archivos a los esclavos y leer hashes. Usa select para ir viendo que fds estan listos
 
     int i = 1;
     fd_set readfdsX;
-    while (i < argc)
+    
+    // Manda un archivo a cada esclavo
+    for (size_t j = 0; j < SLAVE_AMMOUNT && i < argc; j++, i++)
     {
-        // Manda un archivo a cada esclavo
-        for (size_t j = 0; j < SLAVE_AMMOUNT && i < argc; j++)
-        {
-            send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
-            i++;
-        }
+        printf("Sending files\n");
+        send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i])+1, 0);
+    }
+    
+    int h=0;
+
+    while (h < 2)
+    {
+        printf("Selecting readable pipes\n");
+    
         // cargar readfds y writefds con todos los fds monitoreados
         readfdsX = readfds;
-        select(100, &readfdsX, NULL, NULL, NULL);
-        // en readfds y writefds quedan solo los fds habilitados para lectura y escritura respectivamente
-        // read from readable fds (LEER HASHES) y send despues de leer (MANDAR ARCHIVOS)
+        select(FD_SETSIZE, &readfdsX, NULL, NULL, NULL); 
+        // en readfds quedan solo los fds habilitados para lectura
+        printf("Selected readable pipes\n");
 
+        // read from readable fds (LEER HASHES) y send despues de leer (MANDAR ARCHIVOS)
         for (size_t j = 0; j < SLAVE_AMMOUNT && i < argc; j++)
         {
             if (FD_ISSET(slaveToAppFds[j][0], &readfdsX))
             {
+                printf("Reading...\n");
                 // Test
                 char buf[100];
                 read(slaveToAppFds[j][0], buf, 100);
                 printf("%s\n", buf);
                 //
-                send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
-                i++;
+                //send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
+                h++;
             }
         }
     }
--- slave.c
+++ slave.c
@@ -4,11 +4,18 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <wait.h>
+#include <string.h>
 
 #define MD5PATH "./md5.sh"
 #define MAX_CHARS 1000
 
-int main(int argc, char * argv[]) {
+int main() {
+
+    //printf("In parent");
+
+    //Variables de testing
+    size_t amount_read;
     
     //Variables del ciclo inicial
     char line[MAX_CHARS];
@@ -16,21 +23,34 @@ int main(int argc, char * argv[]) {
 
 
     //Variables de los pipes con md5sum
-    char * argvChild[] = {MD5PATH, NULL, NULL};
+    int wstatus;
+    char * argvChild[] = {MD5PATH, line, NULL};
     char * envpChild[] = {NULL};
     char md5_buff[MAX_CHARS];
     int MD5ToSlave[2];
     pid_t slave_pid = getpid();
+
+
     
     //Código
     pipe(MD5ToSlave);
 
+    
+   
+    
     //Loop consistente en: leer de stdin mientras haya algo, procesarlo y enviarlo a stdout (proceso aplicación)
-    while((bytes_read = read(stdin, line, MAX_CHARS)) > 0) { 
+    //while((bytes_read = read(STDIN_FILENO, line, MAX_CHARS)) > 0) {
+    
+        read(STDIN_FILENO, line, MAX_CHARS); //Hace que no se imprima el "In parent" del principio???
+
+        
         if (fork()==0) {
 
-            argvChild[1] = line;
-
+            
+            //printf("In child\n");
+            //printf("Getting md5 of %s\n", line);
+            
+            //Maneja fds
             dup2(MD5ToSlave[1], 1);
             close(MD5ToSlave[1]);
             close(MD5ToSlave[0]);
@@ -38,9 +58,27 @@ int main(int argc, char * argv[]) {
             execve(MD5PATH, argvChild, envpChild);
         }
         else {
+            //Maneja fds
             close(MD5ToSlave[1]);
-            read(MD5ToSlave[0], md5_buff, MAX_CHARS);
-            printf("Slave PID\tFile name\tMD5\n%s\t%s\t%s", slave_pid, line, md5_buff);
+
+            //espera al hijo y lee su info
+            wait(&wstatus);
+            amount_read = read(MD5ToSlave[0], md5_buff, MAX_CHARS);
+            md5_buff[amount_read-1] = '\0';
+
+            //printf("md5 read\n");
+            
+            //Maneja fds
+            close(MD5ToSlave[0]);
+            
+            //printf("Read from md5: %s\n", md5_buff);
+            
+            //Devuelve info a stdout (app)
+            write(STDOUT_FILENO, md5_buff, strlen(md5_buff)); //TODO: Change md5_buff (missing info)
         }
-    }
+        
+    //}
+    
+    
+    return 0;
 }
