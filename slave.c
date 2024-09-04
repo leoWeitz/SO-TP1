5f6cbaad77111dcd8e73ab68c64d17c4dfa34b68
leoWeitz <lweitz@itba.edu.ar>
2024-09-04 18:01:08 -0300

    Fix slave

--- slave.c
+++ slave.c
@@ -6,13 +6,18 @@
 #include <fcntl.h>
 #include <wait.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <stdint.h>
+#include <stdlib.h>
+       
+       
 
-#define MD5PATH "./md5.sh"
+#define MD5PATH "/usr/bin/md5sum"
 #define MAX_CHARS 1000
 
 int main() {
 
-    //printf("In parent");
+   
 
     //Variables de testing
     size_t amount_read;
@@ -29,26 +34,24 @@ int main() {
     char md5_buff[MAX_CHARS];
     int MD5ToSlave[2];
     pid_t slave_pid = getpid();
+    pid_t w, child;
 
 
     
     //Código
-    pipe(MD5ToSlave);
+    
 
     
    
     
     //Loop consistente en: leer de stdin mientras haya algo, procesarlo y enviarlo a stdout (proceso aplicación)
-    //while((bytes_read = read(STDIN_FILENO, line, MAX_CHARS)) > 0) {
-    
-        read(STDIN_FILENO, line, MAX_CHARS); //Hace que no se imprima el "In parent" del principio???
+    while((bytes_read = scanf("%s", line)) > 0) {
 
+        pipe(MD5ToSlave);
         
-        if (fork()==0) {
+        if ((child = fork())==0) {
 
-            
-            //printf("In child\n");
-            //printf("Getting md5 of %s\n", line);
+
             
             //Maneja fds
             dup2(MD5ToSlave[1], 1);
@@ -62,22 +65,42 @@ int main() {
             close(MD5ToSlave[1]);
 
             //espera al hijo y lee su info
-            wait(&wstatus);
-            amount_read = read(MD5ToSlave[0], md5_buff, MAX_CHARS);
-            md5_buff[amount_read-1] = '\0';
+            
 
-            //printf("md5 read\n");
+            w = waitpid(child, &wstatus, 0 );
+                   if (w == -1) {
+                       perror("waitpid");
+                       exit(EXIT_FAILURE);
+                   }
+
+                   if (WIFEXITED(wstatus)) {
+                       printf("exited, status=%d\n", WEXITSTATUS(wstatus));
+                   } else if (WIFSIGNALED(wstatus)) {
+                       printf("killed by signal %d\n", WTERMSIG(wstatus));
+                   } else if (WIFSTOPPED(wstatus)) {
+                       printf("stopped by signal %d\n", WSTOPSIG(wstatus));
+                   } else if (WIFCONTINUED(wstatus)) {
+                       printf("continued\n");
+                   }
+
+
+            amount_read = read(MD5ToSlave[0], md5_buff, MAX_CHARS);
+            
+            //Muy feo pero funciona
+            int i;
+            for (i = 0; md5_buff[i] != '\n'; i++);
+            md5_buff[i] = 0;
             
             //Maneja fds
             close(MD5ToSlave[0]);
             
-            //printf("Read from md5: %s\n", md5_buff);
-            
             //Devuelve info a stdout (app)
-            write(STDOUT_FILENO, md5_buff, strlen(md5_buff)); //TODO: Change md5_buff (missing info)
+            printf("%s\n", md5_buff); //TODO: Change md5_buff (missing info)
+
+
         }
         
-    //}
+    }
     
     
     return 0;
