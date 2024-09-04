1be0f46516567463aaa3abb6b1b5f9744c074507
Fedir03 <federicoruckauf@gmail.com>
2024-09-03 23:05:16 -0300

    Modularizacion del proceso padre y cambio de send a write. Falta pulir tema buffer.

--- app.c
+++ app.c
@@ -6,9 +6,21 @@
 #include <sys/socket.h>
 #include <string.h>
 
+#define INITIAL_PATH_AMMOUNT 4 // Cambiar este define o el de slave ammount. Algo tiene que depender de cuantos archivos mandan. Tiene que ser una variable.
 // Informacion de slave
 #define SLAVE_AMMOUNT 10
 #define SLAVEPATH "./slave.o"
+char *const argvSlave[] = {SLAVEPATH, NULL};
+char *const envpSlave[] = {NULL};
+
+int currentPath = 1;
+int slavesCreated = 0;
+
+char *addPath(char buf[], char const *argv[], int argc);
+
+void closePreviousPipes(int appToSlaveFds[SLAVE_AMMOUNT][2], int slaveToAppFds[SLAVE_AMMOUNT][2]);
+
+void prepareAndExecSlave(int appToSlaveFds[2], int slaveToAppFds[2]);
 
 int main(int argc, char const *argv[])
 {
@@ -17,8 +29,6 @@ int main(int argc, char const *argv[])
     int appToSlaveFds[SLAVE_AMMOUNT][2], slaveToAppFds[SLAVE_AMMOUNT][2];
 
     // Argumentos para execve
-    char *const argvSlave[] = {SLAVEPATH, NULL};
-    char *const envpSlave[] = {NULL};
 
     // Esto es para el select
     fd_set readfds;
@@ -30,50 +40,55 @@ int main(int argc, char const *argv[])
         printf("Piping number %ld\n", i);
         pipe(appToSlaveFds[i]);
         pipe(slaveToAppFds[i]);
+
         if (fork() == 0)
         {
-            close(appToSlaveFds[i][1]);
-            close(slaveToAppFds[i][0]);
-            dup2(appToSlaveFds[i][0], 0);
-            //dup2(slaveToAppFds[i][1], 1);
-            close(appToSlaveFds[i][0]);
-            close(slaveToAppFds[i][1]);
-            execve(SLAVEPATH, argvSlave, envpSlave);
+            prepareAndExecSlave(appToSlaveFds[i], slaveToAppFds[i]);
         }
+        close(appToSlaveFds[i][0]);
+        close(slaveToAppFds[i][1]);
+
         // carga readfds con todos los fds que necesitamos monitorear
         FD_SET(slaveToAppFds[i][0], &readfds);
     }
 
-    //Testing
-    for (size_t j = 0; j < SLAVE_AMMOUNT ; j++) {
+    // Testing
+    for (size_t j = 0; j < SLAVE_AMMOUNT; j++)
+    {
 
-        if (FD_ISSET(slaveToAppFds[j][0], &readfds)) {
+        if (FD_ISSET(slaveToAppFds[j][0], &readfds))
+        {
             printf("%d\n", slaveToAppFds[j][0]);
         }
     }
     //
 
-    // Loop para mandar archivos a los esclavos y leer hashes. Usa select para ir viendo que fds estan listos
-
     int i = 1;
     fd_set readfdsX;
-    
+
     // Manda un archivo a cada esclavo
     for (size_t j = 0; j < SLAVE_AMMOUNT && i < argc; j++, i++)
     {
+        char *buffer;
         printf("Sending files\n");
-        send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i])+1, 0);
-    }
-    
-    int h=0;
+        for (size_t i = 0; i < INITIAL_PATH_AMMOUNT; i++)
+        {
+            buffer = addPath(buffer, argv, argc);
+        }
 
+        write(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]) + 1);
+    }
+
+    int h = 0;
+
+    // Loop para mandar archivos a los esclavos y leer hashes. Usa select para ir viendo que fds estan listos
     while (h < 2)
     {
         printf("Selecting readable pipes\n");
-    
+
         // cargar readfds y writefds con todos los fds monitoreados
         readfdsX = readfds;
-        select(FD_SETSIZE, &readfdsX, NULL, NULL, NULL); 
+        select(FD_SETSIZE, &readfdsX, NULL, NULL, NULL);
         // en readfds quedan solo los fds habilitados para lectura
         printf("Selected readable pipes\n");
 
@@ -84,15 +99,52 @@ int main(int argc, char const *argv[])
             {
                 printf("Reading...\n");
                 // Test
-                char buf[100];
+                char *buf;
+                buf = addPath(buf, argv, argc);
                 read(slaveToAppFds[j][0], buf, 100);
                 printf("%s\n", buf);
                 //
-                //send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
+                // send(appToSlaveFds[j][1], argv[i], /*Medio feo esto, chequear*/ strlen(argv[i]), 0);
                 h++;
             }
         }
     }
 
     return 0;
+}
+
+char *addPath(char buf[], char const *argv[], int argc)
+{
+    if (currentPath >= argc)
+    {
+        return NULL;
+    }
+    char *newBuf = strcat(buf, '\n');
+    newBuf = strcat(newBuf, argv[currentPath]);
+    newBuf = strcat(newBuf, '\0');
+    return newBuf;
+}
+
+void closePreviousPipes(int appToSlaveFds[SLAVE_AMMOUNT][2], int slaveToAppFds[SLAVE_AMMOUNT][2])
+{
+    for (size_t i = 0; i < slavesCreated; i++)
+    {
+        close(appToSlaveFds[i][0]);
+        close(appToSlaveFds[i][1]);
+        close(slaveToAppFds[i][0]);
+        close(slaveToAppFds[i][1]);
+    }
+}
+
+void prepareAndExecSlave(int appToSlaveFds[2], int slaveToAppFds[2])
+{
+    close(appToSlaveFds[1]);
+    close(slaveToAppFds[0]);
+    dup2(appToSlaveFds[0], 0);
+    dup2(slaveToAppFds[1], 1);
+    close(appToSlaveFds[0]);
+    close(slaveToAppFds[1]);
+    closePreviousPipes(appToSlaveFds, slaveToAppFds);
+    slavesCreated++;
+    execve(SLAVEPATH, argvSlave, envpSlave);
 }
